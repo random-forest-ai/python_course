3d51c90d35d9c9271ff43c0381b4b36803a541ae
random-forest <random-forest@random-forest.local>
2021-07-10 00:17:48 +0800

    Chapter 1 Code

---
+++ Chapter_1/Hello_World_Chinese.py
@@ -0,0 +1,2 @@
+if __name__ == '__main__':
+    print("Hello請注視面前的我><")
---
+++ Chapter_1/Hello_World_multiple_lines.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    # print on three different lines
+    # print("Hello World!")
+    # print("Hello World too!")
+    # print("Hello World three!")
+
+    # invoke print on one line
+    # print("Hello World!", "Hello World too!", "Hello World three!")
+
+    # invoke print on one line with separator
+    print("Hello World!", "Hello World too!", "Hello World three!", sep=',')
