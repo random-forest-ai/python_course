eff3f15257548bd15425691f5ba4037c575f3a38
random-forest <random-forest@random-forest.local>
2021-07-10 22:53:33 +0800

    Include Chapter 2 comments

---
+++ Chapter_2/Comments/Comments.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    # This is a line printing Hello World
+    # This is a second line comment
+    '''
+    This is a line printing Hello World
+    This is a second line comment
+    '''
+    print("Hello World")
+
